a29b1d44355bd476571f486cf1d4c007294f5291
Matthew Young <myoung89@uncc.edu>
2021-06-17 17:15:19 -0400

    Add FK relations for tables.

--- RMData/dbo/Tables/SaleDetail.sql
+++ RMData/dbo/Tables/SaleDetail.sql
@@ -5,5 +5,7 @@
     [ProductId] INT NOT NULL, 
     [PurchasePrice] MONEY NOT NULL, 
     [Tax] MONEY NOT NULL DEFAULT 0, 
-    [Quantity] INT NOT NULL DEFAULT 1
+    [Quantity] INT NOT NULL DEFAULT 1, 
+    CONSTRAINT [FK_SaleDetail_ToSale] FOREIGN KEY ([SaleId]) REFERENCES [Sale]([Id]), 
+    CONSTRAINT [FK_SaleDetail_ToProduct] FOREIGN KEY ([ProductId]) REFERENCES [Product]([Id])
 )
