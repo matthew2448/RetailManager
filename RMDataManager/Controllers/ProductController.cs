6504f0e60fe7ca7cfbdbbbbfeb8a6e6af85cf57b
Matthew Young <myoung89@uncc.edu>
2021-06-28 19:16:20 -0400

    Add User Roles

--- RMDataManager/Controllers/ProductController.cs
+++ RMDataManager/Controllers/ProductController.cs
@@ -10,7 +10,7 @@ using System.Web.Http;
 
 namespace RMDataManager.Controllers
 {
-    [Authorize]
+    [Authorize(Roles = "Cashier,Manager,Admin")]
     public class ProductController : ApiController
     {
         public List<ProductModel> Get()
