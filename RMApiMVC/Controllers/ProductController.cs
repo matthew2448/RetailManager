9437d73b8999ed2e1e4adca4a4837f9537d94ef4
Matthew Young <myoung89@uncc.edu>
2021-07-12 20:43:46 -0400

    (1/2) Upgrading to .NET Core

---
+++ RMApiMVC/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RMDataManager.Library.DataAccess;
+using RMDataManager.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RMApiMVC.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Roles = "Cashier")]
+    public class ProductController : ControllerBase
+    {
+        public List<ProductModel> Get()
+        {
+            //Grabs the user's unique identity 
+            ProductData data = new ProductData();
+            return data.GetProducts();
+        }
+    }
+}
